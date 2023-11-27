b7444042d9a80931b70a1154caa2ca9d91e122e6
KoalaTreeHotel <125790817+KoalaTreeHotel@users.noreply.github.com>
2023-11-27 09:33:37 +0000

    fixing missing file

---
+++ generic-find-and-replace-file.pl
@@ -0,0 +1,50 @@
+#!/usr/bin/perl -w
+
+# runs with Strawberry Perl: http://strawberryperl.com/
+
+use strict;
+use warnings;
+use feature qw(say);
+use File::Find; 
+use File::Basename;
+use Cwd;
+use POSIX qw(floor);
+
+my @content;
+my $counter;
+
+# find all files from current and sub directories
+find( \&fileWanted, '.'); 
+
+# replace text
+$counter = 0;
+foreach my $fileIn (@content) {
+    # input file into array
+    open my $handleIn, '<', $fileIn or die "Could not open $fileIn: $!";
+    chomp (my @linesIn = <$handleIn>);
+    close $handleIn;
+    my @linesOut;
+    # process
+    foreach my $line (@linesIn) {
+        $line =~ s/Path="\.\/DorsaLista"/Path="\.\/Dorsalista"/g;
+        push @linesOut, $line;
+    }
+    # write processed array to file
+    my $fileOut = "$fileIn".".new";    # processed lines to a new file
+    #my $fileOut = $fileIn;              # processed lines to the original file
+    open my $handleOut, '>', $fileOut or die "Could not open $fileOut: $!";
+    foreach (@linesOut) {
+        say $handleOut $_;
+    }
+    close $handleOut;
+}
+
+exit;
+
+sub fileWanted{
+    my $file = $File::Find::name;
+    if ($file =~ /\.xml$/){
+        push @content, $file;
+    }
+}
+
