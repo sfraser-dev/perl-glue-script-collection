c9b893fe242bf48007c734f918d1e2fc94bd04da
KoalaTreeHotel <125790817+KoalaTreeHotel@users.noreply.github.com>
2023-12-05 13:34:25 +0000

    rewrite so Array::Utils module not used

--- generic-list-and-compare-files-in-different-folders.pl
+++ generic-list-and-compare-files-in-different-folders.pl
@@ -1,15 +1,14 @@
-#!/usr/bin/perl -w
+# runs with Strawberry Perl: http://strawberryperl.com/
 
 # runs with Strawberry Perl: http://strawberryperl.com/
+# Uses hash tables (%folder1 and %folder2) to store unique elements instead
+# of arrays, and it checks for differences manually without using Array::Utils.
 
 use strict;
 use warnings;
 use feature qw(say);
 use File::Find; 
 use File::Basename;
-use Cwd;
-use POSIX qw(floor);
-use Array::Utils qw(:all); # install via: cpan Array::Utils
 
 my @content1;
 my @content2;
@@ -20,65 +19,62 @@ my $fileDir;
 my $ext;
 my $filePath;
 my $fileName;
-my $frameCount;
 my $logFile;
 my $perlFilenameBase;
 my $fh;
 my $fileCount;
 
 # create a log file (same name as this file but with .log extension)
-$perlFilenameBase=basename($0);
-$perlFilenameBase=~s/\.pl//;
+$perlFilenameBase = basename($0);
+$perlFilenameBase =~ s/\.pl//;
 $logFile = "$perlFilenameBase\.log";
 open ($fh, '>', $logFile) or die ("Could not open file '$logFile' $!");
 say $fh "fileCount, filePath, fileName";
-$fileCount=0;
+$fileCount = 0;
 
-# find all files from current and sub directories
+# find all files from current and subdirectories
 my @paths;
 
-my $path1 = "F:\\dev7_2016_10p4\\GitRepo_J\\vss2016_10p4_vstudio2015\\bin\\release\\DorsaLista\\";
-my @folder1;
-find( \&fileWanted1, $path1); 
+my $path1 = "C:\\Users\\toepo\\local\\git-weebucket\\perl-glue-script-collection\\test1\\";
+my %folder1;
+find(\&fileWanted1, $path1);
 foreach $fileFound (@content1) {
     # get filename, directory and extension of the found file
     $fileCount++;
-    ($name,$fileDir,$ext) = fileparse($fileFound,'\..*');
+    ($name, $fileDir, $ext) = fileparse($fileFound, '\..*');
     $fileDir =~ s/\//\\/g;
-    $filePath="$fileDir";
-    $fileName="$name$ext";
-    $theFile=$filePath.$fileName;
+    $filePath = "$fileDir";
+    $fileName = "$name$ext";
+    $theFile = $filePath.$fileName;
     chomp $fileCount;
     chomp $filePath;
     chomp $fileName;
     say $fh "$fileCount, $fileName";
-    push @folder1, $fileName;
+    $folder1{$fileName} = 1;
 }
-
 
 say $fh "-------------------------------";
 
-my @folder2;
-my $path2 = "C:\\VitecC9SDK\\VM4C9_1_05_03\\extractHere\\DL_vm4c9sdk_v01.05.03Copy\\bin\\DorsaLista";
-find( \&fileWanted2, $path2); 
+my %folder2;
+my $path2 = "C:\\Users\\toepo\\local\\git-weebucket\\perl-glue-script-collection\\test2\\";
+find(\&fileWanted2, $path2);
 foreach $fileFound (@content2) {
     # get filename, directory and extension of the found file
     $fileCount++;
-    ($name,$fileDir,$ext) = fileparse($fileFound,'\..*');
+    ($name, $fileDir, $ext) = fileparse($fileFound, '\..*');
     $fileDir =~ s/\//\\/g;
-    $filePath="$fileDir";
-    $fileName="$name$ext";
-    $theFile=$filePath.$fileName;
+    $filePath = "$fileDir";
+    $fileName = "$name$ext";
+    $theFile = $filePath.$fileName;
     chomp $fileCount;
     chomp $filePath;
     chomp $fileName;
     say $fh "$fileCount, $fileName";
-    push @folder2, $fileName;
+    $folder2{$fileName} = 1;
 }
 
-
 say $fh "----------- differences --------------------";
-my @theDiff = array_diff(@folder1, @folder2);
+my @theDiff = grep { !$folder1{$_} } keys %folder2;
 foreach my $fileDiff (@theDiff) {
     say $fh $fileDiff;
     say $fileDiff;
@@ -87,13 +83,13 @@ foreach my $fileDiff (@theDiff) {
 close($fh);
 exit;
 
-sub fileWanted1{
+sub fileWanted1 {
     my $file = $File::Find::name;
     push @content1, $file;
     return;
 }
 
-sub fileWanted2{
+sub fileWanted2 {
     my $file = $File::Find::name;
     push @content2, $file;
     return;
